31d6135d467569c298776694daa3bbee6ebf2848
manmohanjina <manish63singh@gmail.com>
2023-03-10 23:41:01 +0530

    variable declaration in ts

---
+++ TypeScript/vari.ts
@@ -0,0 +1,15 @@
+ //declaring a  diffrent variables in ts;
+
+ let no:number=123;
+ console.log(no)
+
+ let str:string='Manish'
+ str.charAt(0)
+ console.log(str);
+
+ const user={
+    name:"manish",
+    age:26
+ }
+ 
+ 
