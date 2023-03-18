60265c873d450de0b21f168db34dcb0b92c6a54c
manmohanjina <manish63singh@gmail.com>
2023-03-18 21:21:51 +0530

    update

--- leetcode/lsum.js
+++ leetcode/lsum.js
@@ -8,38 +8,38 @@
 
 //find ratio of arr elm;
 
-let arr = [-4 ,3 ,-9, 0, 4 ,1];
-let n=arr.length
+let arr = [-4, 3, -9, 0, 4, 1];
+let n = arr.length;
 
- let pad={};
- for(let i=0;i<arr.length;i++){
-    if(pad[arr[i]]==undefined){
-        pad[arr[i]]=1
-    }
-    else{
-        pad[arr[i]]++
-    }
- }
- let ans=[]
- let positive=0;
- let negative=0;
- let nutral=0
- for(let  key in pad){
- 
-    if(Number(key)>0){
-       positive+=Number(pad[key])
-       arr.push(positive)
-       
-    }
-    if(Number(key)==0){
-       nutral+=Number(pad[key])
-      arr.push(nutral)
-       
-    }
-    if(Number(key)<0){
-        negative+=Number(pad[key])
-        arr.push(negative)
-    }
- }
- 
-console.log(positive,negative,nutral)
+let pad = {};
+for (let i = 0; i < arr.length; i++) {
+  if (pad[arr[i]] == undefined) {
+    pad[arr[i]] = 1;
+  } else {
+    pad[arr[i]]++;
+  }
+}
+let ans = [];
+let positive = 0;
+let negative = 0;
+let nutral = 0;
+for (let key in pad) {
+  if (Number(key) > 0) {
+    positive += Number(pad[key]);
+    arr.push(positive);
+  }
+  if (Number(key) == 0) {
+    nutral += Number(pad[key]);
+    arr.push(nutral);
+  }
+  if (Number(key) < 0) {
+    negative += Number(pad[key]);
+    arr.push(negative);
+  }
+}
+
+let ratio1 = positive / n
+let r2 = negative / n
+let r3 = nutral / n
+
+console.log(ratio1.toFixed(6), r2.toFixed(6), r3.toFixed(6));
---
+++ leetcode/pat.js
@@ -0,0 +1,16 @@
+//prinitng pattern ;
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+
+let n=4;
+for(let i=0;i<n;i++){
+    let bag="";
+    for(let j=i;j>=0;j--){
+       
+    }
+    console.log(bag)
+}
