58669b2211c052750b48898c2e4579dca8379671
manmohanjina <manish63singh@gmail.com>
2023-03-20 23:13:52 +0530

    update

--- cypressAutomation/cypressautomation/cypress/e2e/spec.cy.js
+++ cypressAutomation/cypressautomation/cypress/e2e/spec.cy.js
@@ -3,34 +3,38 @@ describe("Testing the counter application", () => {
   //without actully chagning the existing database we have to test our application;
 
   it("should load up the application", () => {
-    cy.visit("http://localhost:3001/");
+    cy.visit("http://localhost:3000");
   });
 
   //counter variable should be on the ui;
   //we can give testid's to our components to verify that it is present in the dom tree or not
 
-  it("should show 0 on first render of our app", () => {
+  it("should show  counter value set in interceptor", () => {
     //we should not modify the db, but we can intercept the req made to the db;
-
-    cy.intercept("GET", "http://localhost:3001/counter", { count: 200 }).as(
-      "counterReq"
+    cy.intercept("GET", "http://localhost:3000/counter", { count: 200 }).as(
+      "countReq"
     );
-    cy.visit("http://localhost:3001/counter");
-    cy.wait("@counterReq");
-
+    cy.visit("http://localhost:3000");
+    cy.wait("@countReq");
     cy.get("[data-testid=count-div]").should("be.visible");
+    cy.get("[data-testid=count-div]").should("have.text", "count:200");
+
     //the value of count should be dynamic;
     //or we can send the fake data ;
 
-    cy.get("[data-testid=count-div]").should("have.text", "count:200");
+    // cy.get("[data-testid=count-div]").should("have.text", "count:200");
   });
 
   //btn should be also visible on the ui;
 
-  it.skip("btn is there or not ", () => {
-    cy.visit("http://localhost:3000/counter");
-    cy.get("[data-testid=btncount]").should("be.visible");
-    cy.get("[data-testid=btncount]").click();
+  it.should("increment the count on clicking the btn", () => {
+    cy.intercept("GET", "http://localhost:3001/counter", { count: 200 }).as(
+      "vari"
+    );
+
+    cy.visit("http://localhost:3001/counter");
+
+    cy.wait("@vari");
   });
 
   //it should mount up on the ui
