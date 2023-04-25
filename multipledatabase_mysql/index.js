fa20abbcfb774a113d4e963c364e281d489d6dd0
manmohanjina <manish63singh@gmail.com>
2023-04-25 08:05:52 +0530

    searching functionality

--- multipledatabase_mysql/index.js
+++ multipledatabase_mysql/index.js
@@ -47,26 +47,7 @@ fs.appendFileSync("myfile.txt", (`${check}:\t\n`), (err) => {
   res.send("ok");
 });
 
-// fs.readFile('path/to/book.txt', 'utf8', (err, data) => {
-//     if (err) throw err;
 
-//     const words = data.split(/\s+/); // split the book contents into an array of words
-//     const wordsCount = {}; // an object to store the word count
-
-//     // loop through the words and count them
-//     for (let i = 0; i < words.length; i++) {
-//       const word = words[i].toLowerCase().replace(/[^a-zA-Z]/g, ''); // convert to lowercase and remove non-alphabetic characters
-//       if (!word) continue; // skip empty words
-//       if (!wordsCount[word]) wordsCount[word] = { count: 0, pages: [] }; // initialize the word count and page array
-//       wordsCount[word].count++; // increment the word count
-//       wordsCount[word].pages.push(Math.floor(i/300)+1); // add the page number (assuming 300 words per page)
-//     }
-
-//     // log the word count and page numbers
-//     for (const word in wordsCount) {
-//       console.log(`${word}: ${wordsCount[word].count} (${wordsCount[word].pages.join(', ')})`);
-//     }
-//   });
 
 app.listen(8080, () => {
   console.log("server running at port 8080");
